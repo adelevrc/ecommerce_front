0d402f289548af186f3d51ab5ee2e82932d91a4e
adelevrc <adele.vercaygne@gmail.com>
2021-03-25 10:05:36 +0100

    fix userRole when null  in AnimalList

--- src/components/Animals/AnimalsList.js
+++ src/components/Animals/AnimalsList.js
@@ -10,9 +10,8 @@ const AnimalsList = () => {
 
     let API_URL = process.env.REACT_APP_API_URL; 
     const [animals, setAnimals] = useState([]);
-    const tokenString = localStorage.getItem('token');
-    const userToken = JSON.parse(tokenString);
-    const userRole = userToken.userRole;
+    const [isAdmin, setisAdmin] = useState(false); 
+  
 
     useEffect(() =>{
         axios.get(`${API_URL}/animals`)
@@ -22,9 +21,23 @@ const AnimalsList = () => {
         .catch (err => console.log(err)); 
     }, [API_URL]);
 
-    console.log(animals); 
+    useEffect(() =>{
+        if(localStorage.getItem('token') != null)
+        checkIsAdmin(); 
+    }, []);
+
+
+    const checkIsAdmin = () => {
+        const tokenString = localStorage.getItem('token');
+        const userToken = JSON.parse(tokenString);
+        if(userToken === null){setisAdmin(false)}
+        if(userToken.userRole === null){setisAdmin(false)}
+        if(userToken.userRole === 'admin'){setisAdmin(true)}
+    }
 
     const deleteAnimal = (animal) => {
+        const tokenString = localStorage.getItem('token');
+        const userToken = JSON.parse(tokenString);
         axios.delete(`${API_URL}/animals/${animal._id}`,
         {
             headers:{'Authorization': 'Bearer ' + userToken.token}
@@ -53,9 +66,9 @@ const AnimalsList = () => {
                                 <button> Voir </button>
                             </Link>
                             <Link to={`/modification/admin/animals/${animal._id}`}>
-                                <button className={`btn-update ${userRole==="admin" ? '' : 'not-auth-component'}`}> Modifier </button>
+                                <button className={`btn-update ${isAdmin ? '' : 'not-auth-component'}`}> Modifier </button>
                             </Link>
-                            <button className={`btn-delete ${userRole==="admin" ? '' : 'not-auth-component'}`}onClick={() =>deleteAnimal(animal)}> Supprimer </button>
+                            <button className={`btn-delete ${isAdmin ? '' : 'not-auth-component'}`}onClick={() =>deleteAnimal(animal)}> Supprimer </button>
                         </div>
                     </div>
                 )}
